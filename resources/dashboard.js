1b692a1ebe22ab9fe4b940556487af0215ae763f
pozil <philippe.ozil@bonitasoft.com>
2014-09-24 18:50:58 +0200

    Added license header

--- resources/dashboard.js
+++ resources/dashboard.js
@@ -1,9 +1,26 @@
 'use strict';
 /**
- * Based on Fabio Lombardi's original work
- * Edited by Philippe Ozil
+ * Copyright (C) 2014 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
+ 
+/**
+ * Based on Fabio Lombardi's (Bonitasoft) original work
+ * Edited by Philippe Ozil (Bonitasoft)
+ */
+ 
 (function() {
 
 var appModule = angular.module('dashboard', ['ngCookies', 'ui.bootstrap', 'ngBonita']);
